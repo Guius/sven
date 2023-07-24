3a3a22006c936a9dca90184bb95d7c476a8a6304
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2023-07-24 17:06:36 +0100

    ✨ sven now performs vertical movement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Position {
+  x: number;
+  y: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,44 +15,72 @@ export class AppComponent {
 
   snailOrientation: string = './../assets/snail-forwards.png';
 
-  lastSvenPosition: number = 1;
-  currentSvenPosition: number = 1;
+  lastSvenPosition: Position = { x: 1, y: 1 };
+  currentSvenPosition: Position = { x: 1, y: 1 };
 
   ngOnInit() {
     /**
      * first block is gridColumnStart 1; last is gridColumnStart 5
      */
     setInterval(() => {
-      // if sven is at the end of the row, go backwards
-      if (this.currentSvenPosition === 12) {
-        this.lastSvenPosition = this.currentSvenPosition;
-        this.currentSvenPosition -= 1;
-      } else if (this.currentSvenPosition === 1) {
-        // if sven is at the start of the row, go forwards
-        this.lastSvenPosition = this.currentSvenPosition;
-        this.currentSvenPosition += 1;
-      } else {
-        // if he's not at the extremes, sven can choose
-        if (this.getRandomZeroOrOne()) {
-          // 1, so move forward
-          this.lastSvenPosition = this.currentSvenPosition;
-          this.currentSvenPosition += 1;
+      // is sven doing horizontal or vertical movement?
+      if (this.getRandomZeroOrOne()) {
+        // sven is doing horizontal movement
+        // if sven is at the end of the row, go backwards
+        if (this.currentSvenPosition.x === 30) {
+          this.lastSvenPosition.x = this.currentSvenPosition.x;
+          this.currentSvenPosition.x -= 1;
+        } else if (this.currentSvenPosition.x === 1) {
+          // if sven is at the start of the row, go forwards
+          this.lastSvenPosition.x = this.currentSvenPosition.x;
+          this.currentSvenPosition.x += 1;
         } else {
-          // 0, so move backward
-          this.lastSvenPosition = this.currentSvenPosition;
-          this.currentSvenPosition -= 1;
+          // if he's not at the extremes, sven can choose
+          if (this.getRandomZeroOrOne()) {
+            // 1, so move forward
+            this.lastSvenPosition.x = this.currentSvenPosition.x;
+            this.currentSvenPosition.x += 1;
+          } else {
+            // 0, so move backward
+            this.lastSvenPosition.x = this.currentSvenPosition.x;
+            this.currentSvenPosition.x -= 1;
+          }
+        }
+      } else {
+        // sven is doing vertical movement
+        // if sven is at the end of the column, go backwards
+        if (this.currentSvenPosition.y === 30) {
+          this.lastSvenPosition.y = this.currentSvenPosition.y;
+          this.currentSvenPosition.y -= 1;
+        } else if (this.currentSvenPosition.y === 1) {
+          // if sven is at the start of the row, go forwards
+          this.lastSvenPosition.y = this.currentSvenPosition.y;
+          this.currentSvenPosition.y += 1;
+        } else {
+          // if he's not at the extremes, sven can choose
+          if (this.getRandomZeroOrOne()) {
+            // 1, so move forward
+            this.lastSvenPosition.y = this.currentSvenPosition.y;
+            this.currentSvenPosition.y += 1;
+          } else {
+            // 0, so move backward
+            this.lastSvenPosition.y = this.currentSvenPosition.y;
+            this.currentSvenPosition.y -= 1;
+          }
         }
       }
 
-      // find out snail orientation
-      if (this.lastSvenPosition > this.currentSvenPosition) {
+      // find out sven's orientation
+      if (this.lastSvenPosition.x > this.currentSvenPosition.x) {
         this.snailOrientation = './../assets/snail-backwards.png';
       } else {
         this.snailOrientation = './../assets/snail-forwards.png';
       }
 
       document.getElementById('snail-box')!.style.gridColumnStart =
-        this.currentSvenPosition.toString();
+        this.currentSvenPosition.x.toString();
+      document.getElementById('snail-box')!.style.gridRowStart =
+        this.currentSvenPosition.y.toString();
     }, 1000);
   }
 
