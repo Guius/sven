8f43b2b77029312c91dec57bd72edcd0b60ba5af
Guillaume Vitry <guy.vitry@aqua-superpower.com>
2023-07-24 17:45:58 +0100

    ♻️ made movement functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,55 +19,14 @@ export class AppComponent {
   currentSvenPosition: Position = { x: 1, y: 1 };
 
   ngOnInit() {
-    /**
-     * first block is gridColumnStart 1; last is gridColumnStart 5
-     */
     setInterval(() => {
       // is sven doing horizontal or vertical movement?
       if (this.getRandomZeroOrOne()) {
         // sven is doing horizontal movement
-        // if sven is at the end of the row, go backwards
-        if (this.currentSvenPosition.x === 30) {
-          this.lastSvenPosition.x = this.currentSvenPosition.x;
-          this.currentSvenPosition.x -= 1;
-        } else if (this.currentSvenPosition.x === 1) {
-          // if sven is at the start of the row, go forwards
-          this.lastSvenPosition.x = this.currentSvenPosition.x;
-          this.currentSvenPosition.x += 1;
-        } else {
-          // if he's not at the extremes, sven can choose
-          if (this.getRandomZeroOrOne()) {
-            // 1, so move forward
-            this.lastSvenPosition.x = this.currentSvenPosition.x;
-            this.currentSvenPosition.x += 1;
-          } else {
-            // 0, so move backward
-            this.lastSvenPosition.x = this.currentSvenPosition.x;
-            this.currentSvenPosition.x -= 1;
-          }
-        }
+        this.moveHorizontal();
       } else {
         // sven is doing vertical movement
-        // if sven is at the end of the column, go backwards
-        if (this.currentSvenPosition.y === 30) {
-          this.lastSvenPosition.y = this.currentSvenPosition.y;
-          this.currentSvenPosition.y -= 1;
-        } else if (this.currentSvenPosition.y === 1) {
-          // if sven is at the start of the row, go forwards
-          this.lastSvenPosition.y = this.currentSvenPosition.y;
-          this.currentSvenPosition.y += 1;
-        } else {
-          // if he's not at the extremes, sven can choose
-          if (this.getRandomZeroOrOne()) {
-            // 1, so move forward
-            this.lastSvenPosition.y = this.currentSvenPosition.y;
-            this.currentSvenPosition.y += 1;
-          } else {
-            // 0, so move backward
-            this.lastSvenPosition.y = this.currentSvenPosition.y;
-            this.currentSvenPosition.y -= 1;
-          }
-        }
+        this.moveVertical();
       }
 
       // find out sven's orientation
@@ -86,5 +45,69 @@ export class AppComponent {
 
   getRandomZeroOrOne() {
     return Math.round(Math.random()); // Generates either 0 or 1 randomly
+  }
+
+  // HORIZONTAL MOVEMENT
+  moveHorizontal() {
+    // record sven's current position in memory before he moves
+    this.lastSvenPosition.x = this.currentSvenPosition.x;
+    // is he at the extremes of the garden?
+    if (this.currentSvenPosition.x === 31) {
+      this.moveLeft();
+    } else if (
+      this.currentSvenPosition.x === 1 ||
+      this.currentSvenPosition.x === 0
+    ) {
+      this.moveRight();
+    } else {
+      // if he's not at the extremes, sven can choose
+      if (this.getRandomZeroOrOne()) {
+        // 1, so move forward
+        this.moveRight();
+      } else {
+        // 0, so move backward
+        this.moveLeft();
+      }
+    }
+  }
+
+  moveLeft() {
+    this.currentSvenPosition.x -= 1;
+  }
+
+  moveRight() {
+    this.currentSvenPosition.x += 1;
+  }
+
+  // VERTICAL MOVEMENT
+  moveVertical() {
+    // record sven's current position in memory before he moves
+    this.lastSvenPosition.y = this.currentSvenPosition.y;
+    // is he at the extremes of the garden?
+    if (this.currentSvenPosition.y === 30) {
+      this.moveUp();
+    } else if (
+      this.currentSvenPosition.y === 0 ||
+      this.currentSvenPosition.y === 1
+    ) {
+      this.moveDown();
+    } else {
+      // if he's not at the extremes, sven can choose
+      if (this.getRandomZeroOrOne()) {
+        // 1, so move up
+        this.moveUp();
+      } else {
+        // 0, so move down
+        this.moveDown();
+      }
+    }
+  }
+
+  moveDown() {
+    this.currentSvenPosition.y += 1;
+  }
+
+  moveUp() {
+    this.currentSvenPosition.y -= 1;
   }
 }
